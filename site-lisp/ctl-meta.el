73ac728037bd0304e8ebc7a3670265cbcaad6874
Andrew Lowe <alowe756@gmail.com>
2010-09-24 01:18:38 +0000

    via lt

---
+++ site-lisp/ctl-meta.el
@@ -0,0 +1,15 @@
+(require 'nums)
+
+(defun char-ctrl (char)
+	"set control on char"
+	(logior (lsh 1 26) char))
+
+
+(defvar ctl (dec "0x4000000"))
+(defun ctl (c) (+ ctl c))
+
+; maybe the flag key
+(defvar meta (dec "0x800000"))
+(defun meta (c) (+ meta c))
+
+(provide 'ctl-meta)
