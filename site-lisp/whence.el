bbff58a30a3a362b48ae644a2920a32de93a8038
Andrew Lowe <alowe756@gmail.com>
2010-11-29 20:55:18 +0000

    fix whence, remove dependency on get-directory-files

--- site-lisp/whence.el
+++ site-lisp/whence.el
@@ -1,19 +1,19 @@
 (put 'whence 'rcsid 
  "$Id: whencepath.el 8 2010-10-05 01:38:57Z svn $")
 
-(require 'directories)
-
 ; just to remove dependency on ... whencepath
+(defvar whence-executable-terminator (if (eq window-system 'w32) "\\(.exe\\)$" "$") "regexp to match executable files")
+
 (defun whence (thing &optional path)
   "return location of THING along optional PATH
 if specified, PATH is a list of directories.  default is split $PATH
 "
   (loop 
    with y = nil
-   with l = (split-string (getenv "PATH") path-separator t)
-   with pat = (concat thing "$")
-   for x in l when (setq y (get-directory-files x t pat)) return (car y)
+   with l = exec-path
+   with pat = (concat thing whence-executable-terminator)
+   for x in l when (setq y (directory-files x t pat)) return (car y)
    )
   )
-
+; (whence "pwd")
 (provide 'whence)
