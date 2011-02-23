f759a3bbe42cc1f9b8e814379ef31a0978aa06e1
Andrew Lowe <alowe756@gmail.com>
2011-02-23 14:11:07 +0000

    factor and rationalize trim-*-string trim-*-region add numerous unit tests

--- site-lisp/trim.el
+++ site-lisp/trim.el
@@ -3,90 +3,230 @@
 
 (require 'long-comment)
 
-(defun trim-trailing-white-space (&optional s) (interactive)
-  " trim trailing white space from STRING"
-  (if (interactive-p)
-      (while (search-forward "[ 	]*$" nil t)
-	(replace-match "" nil t))
-    (and s 
-	 (let* ((p (string-match "[ 	]$" s)))
-	   (substring s 0 p)))))
+(defvar *leading-whitespace-pattern* "^[ \t]+")
+(defvar *trailing-whitespace-pattern* "[ \t\r]+$")
+(defvar *blank-line-pattern* "[\r\n][\r\n]+")
 
-(defun trim-leading-white-space (&optional s) 
-  " trim leading white space.
-when called from a program, and optional STRING is specified, trim white space from that."
-  (interactive)
-  (if (interactive-p)
-      (save-excursion
-	(while (re-search-forward "^[ 	]+" nil t)
-	  (replace-match "" nil t))
-	)
-    (and s (replace-regexp-in-string "^[\t\n ]+" "" s))
-    )
+(defun trim-trailing-white-space-string (&optional s)
+  (interactive "strim trailing white space from string: ")
+  (and s 
+       (let* ((p (string-match *trailing-whitespace-pattern* s)))
+	 (if p (substring s 0 p) s))
+       )
   )
+; (assert (string= (trim-trailing-white-space-string "foo 	") "foo"))
+; (assert (string= (trim-trailing-white-space-string "foo") "foo"))
 
-(defun trim-white-space (&optional s) 
-  "trim white-space in string.  when called interactively, trims region."
-  (interactive)
-  (if (interactive-p)
-      (save-restriction
-	(narrow-to-region (point) (mark))
-	(goto-char (point-min))
-	(call-interactively 'trim-leading-white-space)
-	(goto-char (point-min))
-	(call-interactively 'trim-trailing-white-space))
-    (if (> (length s) 0)
-	(trim-trailing-white-space (trim-leading-white-space s))
-      s)
-    )
-  )
-(fset 'trim 'trim-white-space)
-
-(defun trim-buffer ()
-  (while (search-forward "[ ]*$" nil t)
-    (replace-match "" nil t))
-  )
-
-(defun trim-region (beg end)
+(defun trim-trailing-white-space-region (&optional beg end)
+  " trim trailing white space from region
+when called from a program, takes two args BEG and END 
+"
   (interactive "r")
+; work backwards as end may move
   (save-excursion
-    (goto-char beg)
-    (while (re-search-forward "[ \t]*$" end t)
-      (replace-match "" nil nil))
+    (goto-char end)
+    (while (re-search-backward *trailing-whitespace-pattern* beg t)
+      (replace-match ""))
     )
   )
+(/*
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo 	")
+	    (trim-trailing-white-space-region (point-min) (point-max))
+	    (buffer-string)
+	    )
+	  "bar baz bo"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo 	\n")
+	    (let ((p (point)))
+	      (insert "binga banga bunga	 \n")
+	      (trim-trailing-white-space-region p (point-max))
+	      (buffer-string)
+	      )
+	    )
+	  "bar baz bo 	\nbinga banga bunga\n"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo 	\n")
+	    (let ((p (point)))
+	      (insert "f\t\n")
+	      (insert "binga banga bunga	 \n")
+	      (trim-trailing-white-space-region (point-min) p)
+	      (buffer-string)
+	      )
+	    )
+	  "bar baz bo\nf\t\nbinga banga bunga	 \n"
+	  )
+	 )
+ */)
+
+(defun trim-trailing-white-space ()
+  "trim trailing white space from `point' to `point-max'
+"
+  (interactive)
+  (trim-trailing-white-space-region (point) (point-max))
+  )
+
+(defun trim-leading-white-space-string (&optional s)
+  (interactive "strim leading white space from string: ")
+  (and s 
+       (let* ((p (string-match *leading-whitespace-pattern* s)))
+	 (if p (substring s (match-end 0)) s))
+       )
+  )
+; (assert (string= (trim-leading-white-space-string " 	foo") "foo"))
+; (assert (string= (trim-leading-white-space-string "foo") "foo"))
+
+(defun trim-leading-white-space-region (&optional beg end)
+  " trim leading white space from region
+when called from a program, takes two args BEG and END 
+"
+  (interactive "r")
+; work backwards as end will move
+  (save-excursion
+    (goto-char end)
+    (while (re-search-backward *leading-whitespace-pattern* beg t)
+      (replace-match ""))
+    )
+  )
+(/*
+ (assert (string=
+	  (with-temp-buffer
+	    (insert " 	bar baz bo 	")
+	    (trim-leading-white-space-region (point-min) (point-max))
+	    (buffer-string)
+	    )
+	  "bar baz bo 	"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert " \tbar baz bo\t \n")
+	    (let ((p (point)))
+	      (insert " \tbinga banga bunga\t \n")
+	      (trim-leading-white-space-region p (point-max))
+	      (buffer-string)
+	      )
+	    )
+	  " \tbar baz bo\t \nbinga banga bunga\t \n"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert " \tbar baz bo\t \n")
+	    (let ((p (point)))
+	      (insert " \tbinga banga bunga\t \n")
+	      (trim-leading-white-space-region (point-min) p)
+	      (buffer-string)
+	      )
+	    )
+	  "bar baz bo\t \n \tbinga banga bunga\t \n"
+	  )
+	 )
+ */)
+
+(defun trim-leading-white-space ()
+  "trim leading white space from `point' to `point-max'
+"
+  (interactive)
+  (trim-leading-white-space-region (point) (point-max))
+  )
+
+(defun trim-white-space-string (&optional s) 
+  "trim white-space in STRING
+"
+  (interactive "strim white space from string: ")
+  (trim-trailing-white-space-string (trim-leading-white-space-string s))
+  )
+; (assert (string= (trim-white-space-string " \tfoo\t ") "foo"))
+
+(defun trim-white-space-region (beg end)
+  "trim white space from region
+when called from a program, requires two args: BEG and END
+"
+  (interactive "r")
+  (trim-leading-white-space-region beg end)
+  (trim-trailing-white-space-region beg end)
+  )
+
+(defun trim-white-space () 
+  "trim white space from `point' to `point-max'
+"
+  (interactive)
+  (trim-white-space-region (point) (point-max))
+  )
+
+(defun trim-blank-lines-string (s)
+  "trim blank lines from STRING
+"
+  (interactive "strim blank lines from string: ")
+  (and s 
+       (replace-regexp-in-string *blank-line-pattern* "\n" s)
+       )
+  )
+; (assert (string= (trim-blank-lines-string "foo\n\nbar")  "foo\nbar"))
 
 (defun trim-blank-lines-region (beg end)
   "trim blank lines from region.
 "
   (interactive "r")
   (save-excursion
-    (goto-char beg)
-    (while (re-search-forward  "[\n]+" end t)
+    (goto-char end)
+    (while (re-search-backward *blank-line-pattern* beg t)
       (replace-match "\n" nil nil))
     )
   )
+(/*
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo\n\n")
+	    (trim-blank-lines-region (point-min) (point-max))
+	    (buffer-string)
+	    )
+	  "bar baz bo\n"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo\n\n")
+	    (let ((p (point)))
+	      (insert "binga banga bunga\n\n")
+	      (trim-blank-lines-region p (point-max))
+	      (buffer-string)
+	      )
+	    )
+	  "bar baz bo\n\nbinga banga bunga\n"
+	  )
+	 )
+ (assert (string=
+	  (with-temp-buffer
+	    (insert "bar baz bo\n\n\n")
+	    (let ((p (point)))
+	      (insert "f\n\n")
+	      (insert "binga banga bunga\n\n")
+	      (trim-blank-lines-region (point-min) p)
+	      (buffer-string)
+	      )
+	    )
+	  "bar baz bo\nf\n\nbinga banga bunga\n\n"
+	  )
+	 )
+ */)
 
-(defun trim-blank-lines (&optional s)
-  "if interactive, `trim-blank-lines-region'
-else from a program, trim blank lines and return optional STRING
+(defun trim-blank-lines ()
+  "trim blank lines from `point' to `point-max'
 "
-  (interactive "P")
-  (cond 
-   ((string* s)
-    (replace-regexp-in-string "[\n]+" "\n" s))
-   (t s)
-   )
+  (interactive)
+
+  (trim-blank-lines-region (point) (point-max))
   )
 
-(/*
- (assert (string= (trim-blank-lines "asdf
-sadf
-
-sadf	asdf") "asdf
-sadf
-sadf	asdf")
-	 )
- )
+(fset 'trim 'trim-white-space-string)
 
 (provide 'trim)
