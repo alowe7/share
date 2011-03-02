b643d3ebc158b65b8b1a8b80f862eb4418902384
Andrew Lowe <alowe756@gmail.com>
2011-03-02 01:13:24 +0000

    if there is a choice between compiled and source versions of the parent, prefer the source

--- site-lisp/config.el
+++ site-lisp/config.el
@@ -382,13 +382,15 @@ default value module is `this-load-file'
   " visit the immediate parent of MODULE
 if there is a choice between compiled and source versions of the parent, prefer the source
 "
+  (interactive)
   (let* (
 	 (module (or module (this-load-file)))
 	 (y (file-name-sans-extension (file-name-nondirectory module)))
-	 (l (config-ancestors module)))
-    (cond
-     ((file-exists-p (setq z (expand-file-name (concat y ".el") (file-name-directory (car l))))) z)
-     (t x))
+	 (l (config-ancestors module))
+	 (parent (cond
+		  ((file-exists-p (setq z (expand-file-name (concat y ".el") (file-name-directory (car l))))) z)
+		  (t x))))
+    (if (and (interactive-p) parent) (find-file parent) parent)
     )
   )
 ; (find-config-parent "c:/home/a/emacs/config/hosts/granite/keys.el")
