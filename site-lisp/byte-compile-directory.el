a37cdd164895afc5e68775a6946b6042cfd73a60
Andrew Lowe <alowe756@gmail.com>
2013-10-10 18:53:01 +0000

    *** empty log message ***

--- site-lisp/byte-compile-directory.el
+++ site-lisp/byte-compile-directory.el
@@ -1,4 +1,4 @@
 (add-to-list 'load-path ".")
-(require 'collect-directories)
+;(require 'collect-directories)
 ;(loop for x in (collect-directories (expand-file-name "lisp/cedet" source-directory)) do (add-to-list 'load-path x))
 (setq byte-compile-warnings '(not cl-functions free-vars unresolved obsolete))
